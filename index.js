3716642fb8ee7c532ca0319f071619fafe8d7a59
Rishal <Rishal.mhd009@gmail.com>
2023-06-13 00:09:47 +0530

    listening on 5000

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
 const cors= require('cors')
+const pool = require('./db')
 
+// psql connection
+pool.connect()
 
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/', (req, res) =>{
+    res.send('yoyy')
+})
+
+
+
+app.listen(5000, ()=> {
+    console.log('listening on 5000');
+})
