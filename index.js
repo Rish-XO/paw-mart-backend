9f0a3f7bed25f137d78d7cf3f0049b1e559e6d89
Rishal <Rishal.mhd009@gmail.com>
2023-06-13 02:09:32 +0530

    create post api

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const express = require('express')
-const app = express()
-const cors= require('cors')
-const pool = require('./db')
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
 
 // psql connection
-pool.connect()
+pool.connect();
 
 app.use(cors());
 app.use(express.json());
 
+// home page
+app.get("/", (req, res) => {
+  res.send("yoyy");
+});
 
-app.get('/', (req, res) =>{
-    res.send('yoyy')
-})
+// create a post
+app.post("/posts/new", async (req, res) => {
+  const { category, breed, price, description } = req.body;
+  const post = await pool.query(
+    "INSERT INTO posts (category, breed, price, description) VALUES ('DOG','PERSIAN','123', 'BOI') RETURNING *"
+  );
+  console.log(post.rows[0]);
+  res.json(post.rows[0])
+});
 
-
-
-app.listen(5000, ()=> {
-    console.log('listening on 5000');
-})
+app.listen(5000, () => {
+  console.log("listening on 5000");
+});
