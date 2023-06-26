c1888d21593e7289282cf4b9dd6d43a43d181edf
Rishal <Rishal.mhd009@gmail.com>
2023-06-26 20:19:30 +0530

    s3

---
+++ s3.js
@@ -0,0 +1,6 @@
+import aws from 'aws-sdk'
+
+const region = "EU (Stockholm) eu-north-1"
+const bucketname = "pawmartbucket"
+const accessKey = process.env.AWS_ACCESSKEY
+const secretAccessKey = process.env.AWS_SECRET_ACCESSKEY
